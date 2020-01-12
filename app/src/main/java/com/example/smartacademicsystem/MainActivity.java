bbcaaa7ed28a9ae623cf4fbeab239ead4858e374
Saifuddin <saifuddin@dict.edu.bd>
2020-01-12 19:05:34 +0600

    changes in design.....

--- app/src/main/java/com/example/smartacademicsystem/MainActivity.java
+++ app/src/main/java/com/example/smartacademicsystem/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smartacademicsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
